d515167b6c69671bfdcde1f5ab91adba467e5fed
barreraalexander <barreraalexander93@gmail.com>
2022-06-20 20:44:23 -0400

    added scss

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,35 +1,23 @@
 <script setup>
-import HeaderNav from './components/navbars/HeaderNav.vue';
-import FooterNav from './components/navbars/FooterNav.vue'
-undefined
+import HeaderNav from '@/components/navbars/HeaderNav.vue';
+import FooterNav from '@/components/navbars/FooterNav.vue'
 
 
 </script>
 
 <template>
-  <HeaderNav/>  
-  <router-view/>
-  <FooterNav/>
+  <section id="MainAppSection">
+    <HeaderNav/>
+    <main>
+      <router-view/>
+    </main>
+    <FooterNav/>
+  </section>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  nav {
-    padding: 30px;
-  }
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+@import "@/assets/scss/base/base.scss";
+@import "@/assets/scss/base/typography.scss";
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
