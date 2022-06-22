a3fea3fcc3db9b3b1e210d3ca7472a1f294fc1f8
barreraalexander <barreraalexander93@gmail.com>
2022-06-22 16:05:03 -0400

    added support for mysql

--- backend/server/database.py
+++ backend/server/database.py
@@ -8,10 +8,13 @@ from os.path import expanduser
 local_config = expanduser("~/etc/kaiyotemoon_config.json")
 launch_config = expanduser("~/etc/kaiyotemoon_config.json")
 
+
+
 with open(local_config) as config_file:
     config = json.load(config_file)
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://%s:%s@%s/%s' % (config.get("SQL_USER"), config.get("SQL_PASSWORD"), config.get("SQL_HOST"), config.get("SQL_DB"))
+MYSQLALCHEMY_DATABASE_URL = 'mysql://%s:%s@%s/%s' % ('root', config.get("SQL_PASSWORD"), config.get("SQL_HOST"), config.get("SQL_DB"))
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- backend/server/models.py
+++ backend/server/models.py
@@ -1,7 +1,9 @@
 from server.database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, Text, DateTime
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
+from datetime import datetime
+
 
 class Poem(Base):
     __tablename__ = 'poems'
@@ -10,3 +12,11 @@ class Poem(Base):
     title = Column(String, nullable=False, unique=True)
     content = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=('now()'))
+
+# class Poem(Base):
+#     __tablename__ = 'poems'
+
+#     id = Column(Integer, primary_key=True, nullable=False)
+#     title = Column(String(50), nullable=False, unique=True)
+#     content = Column(Text(), nullable=False)
+#     created_at = Column(DateTime(), default=datetime.utcnow)
