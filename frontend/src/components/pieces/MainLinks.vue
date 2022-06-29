8b5e862fcdf3ea0717151f69fe7915ce7ebbce91
barreraalexander <barreraalexander93@gmail.com>
2022-06-28 21:43:52 -0400

    update to code, allowing searching

--- frontend/src/components/pieces/MainLinks.vue
+++ frontend/src/components/pieces/MainLinks.vue
@@ -81,14 +81,11 @@ export default {
                 archive_list.style.display = 'flex'
                 for (let archive_li of archive_elements){
                     let li_string = archive_li.innerHTML
-                    let li_list = li_string.toLowerCase().split('')
-                    let li_list_cut = li_list.slice(0, parseInt(li_list.length - li_list.length/2 - 1))
-                    for (let elem_letter of elem){
-                        if (li_list_cut.includes(elem_letter)){
-                            archive_li.style.display = 'block'
-                        } else {
-                            archive_li.style.display = 'none'
-                        }
+                    let leven_dist = this.leven(elem, li_string);
+                    if (leven_dist > 10){
+                        archive_li.style.display = 'block'
+                    } else {
+                        archive_li.style.display = 'none'
                     }
                 }                
             } else {
@@ -98,14 +95,27 @@ export default {
                 }
                 
             }
-
-            // if (event){
-            //     let elem = event.target.value
-
-            //     console.log(elem.value)
-
-            // }
-            console.log(elem)
+        },
+        leven(str1, str2){
+            var track = Array(str2.length + 1).fill(null).map(() =>
+            Array(str1.length + 1).fill(null));
+            for (let i = 0; i <= str1.length; i += 1) {
+                track[0][i] = i;
+            }
+            for (let j = 0; j <= str2.length; j += 1) {
+                track[j][0] = j;
+            }
+            for (let j = 1; j <= str2.length; j += 1) {
+                for (let i = 1; i <= str1.length; i += 1) {
+                    const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
+                    track[j][i] = Math.min(
+                        track[j][i - 1] + 1, // deletion
+                        track[j - 1][i] + 1, // insertion
+                        track[j - 1][i - 1] + indicator, // substitution
+                    );
+                }
+            }
+            return track[str2.length][str1.length];
         }
     }
 }
