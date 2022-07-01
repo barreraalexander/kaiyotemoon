74eb370be01c318b94e10a30111fc62e569a7de4
barreraalexander <barreraalexander93@gmail.com>
2022-07-01 01:40:18 -0400

    ready to test on a server

--- frontend/src/components/pieces/ArchiveList.vue
+++ frontend/src/components/pieces/ArchiveList.vue
@@ -25,7 +25,8 @@ export default {
 
     methods : {
         async handleClick(event, id){
-            let header_nav = document.querySelector('#HeaderNavSection')
+            // let header_nav = document.querySelector('#HeaderNavSection')
+            let header_nav = document.querySelector('#PoemDisplay')
             header_nav.scrollIntoView()
 
             let result = await axios.get(`http://localhost:5001/poems/${id}`)
@@ -34,8 +35,6 @@ export default {
                 let poem_content = document.querySelector('#poem_content')
                 poem_title.innerText = result.data.title
                 poem_content.innerText = result.data.content
-
-                console.log(this.poems_read)
 
                 if (! this.poems_read.includes(poem_title.innerText)){
                     localStorage.poems_read += `$${poem_title.innerText}`
